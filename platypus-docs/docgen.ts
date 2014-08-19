d88799633c1ca08dd7ee6af2f3058d4edf5492d8
Donald Jones <donald@getplatypi.com>
2014-08-19 15:05:34 -0500

    Initial Commit

---
+++ platypus-docs/docgen.ts
@@ -0,0 +1,6 @@
+﻿/// <reference path="typings/tsd.d.ts" />
+var parser = require('comment-parser');
+
+parser.file('./test-data/sample.ts', (err: any, data: any) => {
+    console.log(JSON.stringify(data));
+});
