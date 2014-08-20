ca15bba2f3fc3f876914c114ab8017880c6fd092
Donald Jones <donald@getplatypi.com>
2014-08-19 19:27:17 -0500

    Removed dependency and modified interfaces.

--- platypus-docs/docnodes.ts
+++ platypus-docs/docnodes.ts
@@ -1,27 +1,30 @@
 ﻿module DocNodes {
     export interface INameSpaceNode {
         name: string;
-        parent: INameSpaceNode;
-        namespaces: Array<INameSpaceNode>;
-        classes: Array<IClassNode>;
-        interfaces: Array<IInterfaceNode>;
-        methods: Array<IMethod>;
+        parent?: INameSpaceNode;
+        namespaces?: Array<INameSpaceNode>;
+        classes?: Array<IClassNode>;
+        interfaces?: Array<IInterfaceNode>;
+        methods?: Array<IMethod>;
     }
 
     export interface IClassNode {
         name: string;
-        parent: IClassNode;
-        namespace: INameSpaceNode;
-        interfaces: Array<IInterfaceNode>;
-        methods: Array<IMethod>;
+        parent?: IClassNode;
+        namespace?: INameSpaceNode;
+        interfaces?: Array<IInterfaceNode>;
+        methods?: Array<IMethod>;
     } 
 
     export interface IInterfaceNode {
         name: string;
+        parent?: IInterfaceNode;
+        namespace?: INameSpaceNode;
+        interfaces?: Array<IInterfaceNode>;
+        methods?: Array<IMethod>;
     }
 
     export interface IMethod {
         name: string;
     }
-
 }
