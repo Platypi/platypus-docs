a9465a44cf5c20808f74c6f48e37695f1b4aad9d
Donald Jones <donald@getplatypi.com>
2014-08-21 10:50:47 -0500

    Added remaining logic for node types.

--- platypus-docs/docgen.ts
+++ platypus-docs/docgen.ts
@@ -114,73 +114,115 @@ export module DocGen {
 
                     switch (kind) {
                         case 'function':
-                            if (tmpObj.name) {
-                                //var member = tree[memberof];
-                                var newMethod: DocNodeTypes.IMethodNode = {
-                                    name: tmpObj.name.name,
-                                    description: tmpObj.description.descrption,
-                                    kind: tmpObj.kind.name,
-                                    overrides: (tmpObj.variation ? true : false),
-                                    visibility: tmpObj.access.name,
-                                    static: (tmpObj.static ? true : false),
-                                    remarks: tmpObj.remarks,
-                                    exported: (!tmpObj.exported ? true : false),
-                                    typeparamaters: (tmpObj.typeparam ? tmpObj.typeparam.name : ''),
-                                    returnType: (tmpObj.returns ? tmpObj.returns.type : ''),
-                                    returntypedesc: (tmpObj.returns ? tmpObj.returns.description : ''),
-                                    optional: (tmpObj.optional ? true : false),
-                                    parameters: [],
-                                    published: true
-                                };
+                            // if name is blank, the method is an interface
+                            //var member = tree[memberof];
+                            var newMethod: DocNodeTypes.IMethodNode = {
+                                name: (tmpObj.name ? tmpObj.name.name : ''),
+                                description: tmpObj.description.descrption,
+                                kind: tmpObj.kind.name,
+                                overrides: (tmpObj.variation ? true : false),
+                                visibility: tmpObj.access.name,
+                                static: (tmpObj.static ? true : false),
+                                remarks: tmpObj.remarks,
+                                exported: (!tmpObj.exported ? true : false),
+                                typeparamaters: (tmpObj.typeparam ? tmpObj.typeparam.name : ''),
+                                returnType: (tmpObj.returns ? tmpObj.returns.type : ''),
+                                returntypedesc: (tmpObj.returns ? tmpObj.returns.description : ''),
+                                optional: (tmpObj.optional ? true : false),
+                                parameters: [],
+                                published: true
+                            };
 
-                                // push the params onto the tmpObj
-                                if (tmpObj.params) {
-                                    for (var z = 0; z < tmpObj.params.length; z++) {
-                                        var newParameter: DocNodeTypes.IParameterNode = {
-                                            name: tmpObj.params[z].name,
-                                            kind: tmpObj.params[z].kind,
-                                            description: tmpObj.params[z].description,
-                                            published: true,
-                                            exported: (!tmpObj.exported ? true : false),
-                                        };
-                                        newMethod.parameters.push(newParameter);
-                                    }
+                            // push the params onto the tmpObj
+                            if (tmpObj.params) {
+                                for (var z = 0; z < tmpObj.params.length; z++) {
+                                    var newParameter: DocNodeTypes.IParameterNode = {
+                                        name: tmpObj.params[z].name,
+                                        kind: tmpObj.params[z].kind,
+                                        description: tmpObj.params[z].description,
+                                        published: true,
+                                        exported: (!tmpObj.exported ? true : false),
+                                    };
+                                    newMethod.parameters.push(newParameter);
                                 }
-
-                                (<any>flat.methods).push(newMethod);
-
                             }
+
+                            flat.methods.push(newMethod);
                             break;
                         case 'property':
-                            if (tmpObj.name) {
-                                var newProperty: DocNodeTypes.IPropertyNode = {
-                                    name: tmpObj.name,
-                                    description: tmpObj.description,
-                                    kind: tmpObj.kind.name,
-                                    published: (!tmpObj.published ? true : tmpObj.published),
-                                    exported: (!tmpObj.exported ? true : tmpObj.exported),
-                                    //interface
-                                    //namespace
-                                    //class
-                                    type: tmpObj.type.type,
-                                    remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
-                                    visibility: tmpObj.access.name,
-                                    static: (tmpObj.static ? true : false),
-                                    readonly: (tmpObj.readonly ? true : false),
-                                    //returntypedesc: 
-                                    optional: (tmpObj.optional ? true : false)
-                                };
+                            var newProperty: DocNodeTypes.IPropertyNode = {
+                                name: tmpObj.name.name,
+                                description: tmpObj.description.description,
+                                kind: tmpObj.kind.name,
+                                published: (!tmpObj.published ? true : tmpObj.published),
+                                exported: (!tmpObj.exported ? true : tmpObj.exported),
+                                //interface
+                                //namespace
+                                //class
+                                type: tmpObj.type.type,
+                                remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
+                                visibility: tmpObj.access.name,
+                                static: (tmpObj.static ? true : false),
+                                readonly: (tmpObj.readonly ? true : false),
+                                optional: (tmpObj.optional ? true : false)
+                            };
 
-                                flat.properties.push(newProperty);
-                            }
+                            flat.properties.push(newProperty);
                             break;
                         case 'class':
+                            var newClass: DocNodeTypes.IClassNode = {
+                                name: tmpObj.name.name,
+                                description: tmpObj.description.description,
+                                kind: tmpObj.kind.name,
+                                published: (!tmpObj.published ? true : tmpObj.published),
+                                remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
+                                exported: (!tmpObj.exported ? true : tmpObj.exported),
+                                //parent (extends)
+                                parent: (tmpObj['extends'] ? tmpObj['extends'].type : ''),
+                                //namespace (memberof)
+                                namespace: (tmpObj.namespace ? tmpObj.namespace.type : '')
+                            };
+
+                            //interfaces (implements) treat like params
+                            flat.classes.push(newClass);
                             break;
                         case 'interface':
+                            var newInterface: DocNodeTypes.IInterfaceNode = {
+                                name: tmpObj.name.name,
+                                kind: tmpObj.kind.name,
+                                description: tmpObj.description.description,
+                                published: (!tmpObj.published ? true : tmpObj.published),
+                                exported: (!tmpObj.exported ? true : tmpObj.exported),
+                                remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
+                                //methods
+                            };
+                            flat.interfaces.push(newInterface);
                             break;
                         case 'event':
+                            var newEvent: DocNodeTypes.IEvent = {
+                                name: tmpObj.name.name,
+                                kind: tmpObj.kind.name,
+                                description: tmpObj.description.description,
+                                published: (!tmpObj.published ? true : tmpObj.published),
+                                exported: (!tmpObj.exported ? true : tmpObj.exported),
+                                remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
+                                //class @class
+                                class: tmpObj.class.name
+                            };
+
+                            flat.events.push(newEvent);
                             break;
                         case 'namespace':
+                            var newNamespace: DocNodeTypes.INameSpaceNode = {
+                                name: tmpObj.name.name,
+                                kind: tmpObj.kind.name,
+                                description: tmpObj.description.description,
+                                published: (!tmpObj.published ? true : tmpObj.published),
+                                exported: (!tmpObj.exported ? true : tmpObj.exported),
+                                remarks: (tmpObj.remarks ? tmpObj.remarks.description : ''),
+                            };
+
+                            flat.namespaces.push(newNamespace);
                             break;
                     }
                 }
