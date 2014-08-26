65ceabbae0dcadab32909459fbef91b61899c555
Donald Jones <donald@getplatypi.com>
2014-08-26 12:28:04 -0500

    Modified to match the new public api of docgen

--- platypus-docs/index.ts
+++ platypus-docs/index.ts
@@ -6,4 +6,31 @@ import DocGen = require('./docgen');
 var filename = process.argv[2] || './test-data/sample.ts',
     gen = new DocGen.DocGen.DocGenerator();
 
-gen.fromFile(filename);
+gen.buildTreeFromFile(filename, (tree: any) => {
+    console.log(tree, censor(tree), 4);
+});
+
+
+function censor(censor) {
+    var i = 0;
+
+    return function (key, value) {
+        if (key === 'parent' ||
+            key === 'namespace' ||
+            key === 'class' ||
+            key === 'interface' ||
+            key === 'interfaceNode' ||
+            key === 'namespaceNode' ||
+            key === 'classNode' ||
+            key === 'returntype' ||
+            key === 'method') {
+            if (value && value.name && value.name !== '') {
+                return '[Circular] ' + (value ? value.name : '');
+            } else {
+                return '[Circular] ' + (value ? value.type : '');
+            }
+        }
+
+        return value;
+    };
+}
