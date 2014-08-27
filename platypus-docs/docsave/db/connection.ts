4767cd41bedd72717934352d51242484083b6427
Donald Jones <donald@getplatypi.com>
2014-08-27 16:35:03 -0500

    Connection working.

--- platypus-docs/docsave/db/connection.ts
+++ platypus-docs/docsave/db/connection.ts
@@ -1,2 +1,17 @@
 ﻿/// <reference path="../../typings/tsd.d.ts" />
- 
+import mysql = require('mysql');
+
+var cfg = require('../../../dbconnection.json') || {};
+
+var connection = mysql.createConnection({
+    host: cfg.database.host,
+    user: cfg.database.user,
+    password: cfg.database.pass,
+    database: cfg.database.db
+});
+
+connection.connect();
+
+console.log('connected');
+
+connection.end();
