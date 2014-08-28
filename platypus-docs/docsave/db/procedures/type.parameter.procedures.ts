1ae1cf5ee7f9e075cfac7657d06df54d319208f0
Donald Jones <donald@getplatypi.com>
2014-08-28 15:19:06 -0500

    Added type-parameter procedure.

---
+++ platypus-docs/docsave/db/procedures/type.parameter.procedures.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import base = require('./api.procedures');
+import utils = require('../../../utils/utils');
+import model = require('../models/type.parameter.model');
+
+class TypeParameterProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('TypeParameter');
+    }
+
+    getArgs(parameter: model.ITypeParameter): Array<any> {
+        if (!utils.isObject(parameter)) {
+            return [];
+        }
+
+        return [
+            parameter.interfaceid,
+            parameter.classid,
+            parameter.methodid,
+            parameter.classtypeid,
+            parameter.interfacetypeid,
+            parameter.name,
+            parameter.description,
+            parameter.porder
+        ];
+    }
+}
+
+export = TypeParameterProcedures;
+ 
