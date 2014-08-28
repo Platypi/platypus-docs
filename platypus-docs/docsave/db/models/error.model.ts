1ecf473e7227048fcc41f6094a48577c2e973c9b
Donald Jones <donald@getplatypi.com>
2014-08-28 11:53:20 -0500

    PWS-103 Added error model.

---
+++ platypus-docs/docsave/db/models/error.model.ts
@@ -0,0 +1,11 @@
+﻿export interface IValidationError {
+    message: string;
+    property: string;
+}
+
+export interface IValidationErrors extends Array<IValidationError> { }
+
+export class ValidationError implements IValidationError {
+    constructor(public message: string, public property?: string) { }
+}
+ 
