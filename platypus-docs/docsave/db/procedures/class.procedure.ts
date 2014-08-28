88e960b9fdbee74ef31e13a9c3bd191667d30da5
Donald Jones <donald@getplatypi.com>
2014-08-28 14:23:02 -0500

    PWS-103 #comment Added class procedure.

---
+++ platypus-docs/docsave/db/procedures/class.procedure.ts
@@ -0,0 +1,95 @@
+﻿/// <reference path="../db.d.ts" />
+/// <reference path="../../../typings/tsd.d.ts" />
+
+
+import base = require('./api.procedure');
+import utils = require('../../../utils/utils');
+import model = require('../models/class.model');
+
+class ClassProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('Class');
+    }
+
+    _getAllProcedure() {
+        return this.procedure + 'es';
+    }
+
+    getArgs(c: model.IClass): Array<any> {
+        if (!utils.isObject(c)) {
+            return [];
+        }
+
+        return [
+            c.parentid,
+            c.namespaceid,
+            c.name,
+            c.description,
+            c.example,
+            c.exampleurl,
+            c.remarks,
+            c.exported,
+            c.static,
+            c.registeredtype,
+            c.registeredname
+        ];
+    }
+
+    read(id: number, overloads?: boolean) {
+        return super.read(id, !overloads).then((sets: db.docs.api.rowsets.IGetClassRowSets) => {
+            var inherits = sets[0],
+                namespace = sets[1][0],
+                children = sets[2],
+                interfaces = sets[3],
+                methods = sets[4],
+                methodparameters = sets[5],
+                methodtypeparameters = sets[6],
+                properties = sets[7],
+                typeparameters = sets[8],
+                events = sets[9],
+                c = inherits.pop();
+
+            c.inherits = inherits;
+            c.namespace = namespace;
+            c.children = children;
+            c.interfaces = interfaces;
+            c.methods = this.mergeParameters(methods, methodparameters, methodtypeparameters);
+            c.properties = properties;
+            c.typeparameters = typeparameters;
+            c.events = events;
+            c.namespaceid = c.namespace.id;
+            return c;
+        });
+    }
+
+    mergeParameters(methods: Array<db.docs.api.IMethodsRow>,
+        parameters: Array<db.docs.api.IParametersRow>,
+        typeParameters: Array<db.docs.api.ITypeParameterRow>): Array<db.docs.api.IMethodsRow> {
+
+        var parameter: db.docs.api.IParametersRow,
+            typeParameter: db.docs.api.ITypeParameterRow,
+            mId: number;
+
+        utils.forEach(methods, (method) => {
+            mId = method.id;
+            method.parameters = [];
+            method.typeparameters = [];
+            parameter = parameters[0];
+            typeParameter = typeParameters[0];
+            while (parameters.length > 0 && mId === parameter.id) {
+                method.parameters.push(parameter);
+                parameters.shift();
+                parameter = parameters[0];
+            }
+            while (typeParameters.length > 0 && mId === typeParameter.id) {
+                method.typeparameters.push(typeParameter);
+                typeParameters.shift();
+                typeParameter = typeParameters[0];
+            }
+        });
+
+        return methods;
+    }
+}
+
+export = ClassProcedures;
