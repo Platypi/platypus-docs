5876f40706647342f80a82c4939004bd239e4e81
Donald Jones <donald@getplatypi.com>
2014-08-28 15:04:15 -0500

    PWS-103 #comment Added parameter procedure.

---
+++ platypus-docs/docsave/db/procedures/parameter.procedures.ts
@@ -0,0 +1,41 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../db.d.ts" />
+
+import base = require('./api.procedures');
+import utils = require('../../../utils/utils');
+import model = require('/../../models/parameter.model');
+
+class ParameterProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('Parameter');
+    }
+
+    getArgs(parameter: model.IParameter): Array<any> {
+        if (!utils.isObject(parameter)) {
+            return [];
+        }
+
+        return [
+            parameter.methodid,
+            parameter.name,
+            parameter.type,
+            parameter.methodtypeid,
+            parameter.classtypeid,
+            parameter.interfacetypeid,
+            parameter.description,
+            parameter.defaultvalue,
+            parameter.optional,
+            parameter.porder
+        ];
+    }
+
+    read(id: number) {
+        return super.read(id).then((sets: db.docs.api.rowsets.IGetParameterRowSets) => {
+            var p = sets[0][0];
+            return p;
+        });
+    }
+}
+
+export = ParameterProcedures;
+ 
