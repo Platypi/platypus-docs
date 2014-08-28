b1212878dce8870aca54d8245bafae3498236a77
Donald Jones <donald@getplatypi.com>
2014-08-28 14:51:50 -0500

    Added interface and interface-interface procedures.

---
+++ platypus-docs/docsave/db/procedures/interface.interface.procedure.ts
@@ -0,0 +1,34 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import base = require('./api.procedure');
+import utils = require('../../../utils/utils');
+
+class InterfaceInterfaceProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('InterfaceInterface');
+    }
+
+    getArgs(i: any) {
+        if (!utils.isObject(i)) {
+            return [];
+        }
+
+        return [
+            i.interfaceid,
+            i.extendsinterfaceid
+        ];
+    }
+
+    create(obj: any): Thenable<number> {
+        if (!utils.isObject(obj)) {
+            return Promise.resolve(null);
+        }
+        return this.callProcedure('Insert' + this.procedure, this.getArgs(obj)).then(() => {
+            return obj;
+        });
+    }
+}
+
+
+export = InterfaceInterfaceProcedures;
+ 
---
+++ platypus-docs/docsave/db/procedures/interface.procedure.ts
@@ -0,0 +1,51 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path="../db.d.ts" />
+
+
+import base = require('./api.procedure');
+import utils = require('../../../utils/utils');
+import model = require('../models/interface.model');
+
+class InterfaceProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('Interface');
+    }
+
+    getArgs(i: model.IInterface) {
+        if (!utils.isObject(i)) {
+            return [];
+        }
+
+        return [
+            i.namespaceid,
+            i.name,
+            i.description,
+            i.remarks,
+            i.exported,
+            i.registeredtype,
+            i.registeredname
+        ];
+    }
+
+    read(id: number, overloads?: boolean) {
+        return super.read(id, !overloads).then((sets: db.docs.api.rowsets.IGetInterfaceRowSets) => {
+            var interface = sets[0],
+                inherits = sets[1],
+                namespace = sets[2][0],
+                methods = sets[3],
+                properties = sets[4],
+                typeparameters = sets[5],
+                i = interface.pop();
+
+            i.inherits = inherits;
+            i.namespace = namespace;
+            i.methods = methods;
+            i.properties = properties;
+            i.typeparameters = typeparameters;
+            return i;
+        });
+    }
+}
+
+export = InterfaceProcedures;
+ 
