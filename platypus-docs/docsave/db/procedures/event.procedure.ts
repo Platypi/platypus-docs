ba53d2cf4ce8a360b7e4c87d03888dc76ffd31bc
Donald Jones <donald@getplatypi.com>
2014-08-28 14:51:34 -0500

    PWS-103 #comment Added Event Procedure

---
+++ platypus-docs/docsave/db/procedures/event.procedure.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import base = require('./api.procedure');
+import utils = require('../../../utils/utils');
+import model = require('../models/event.model');
+
+class EventProcedures extends base.ApiProcedures<any> {
+    constructor() {
+        super('Event');
+    }
+
+    getArgs(ev: model.IEvent) {
+        if (!utils.isObject(ev)) {
+            return [];
+        }
+
+        return [
+            ev.classid,
+            ev.name,
+            ev.description,
+            ev.remarks,
+            ev.example,
+            ev.exampleurl
+        ];
+    }
+}
+
+export = EventProcedures;
+ 
