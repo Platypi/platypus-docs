31ea6c1c48debf5746ffd2da607c9e275b4fd1d3
Donald Jones <donald@getplatypi.com>
2014-08-28 11:53:05 -0500

    Added api model.

---
+++ platypus-docs/docsave/db/models/api.model.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import base = require('./base.model');
+
+export class ApiModel<T extends base.IBaseModel> extends base.BaseModel<T> { }
+
+export interface IBaseDoc extends base.IBaseModel {
+
+}
+
+export interface IApiModel extends base.IBaseModel {
+    id?: number;
+    name?: string;
+    description?: string;
+    published?: boolean;
+}
+
+export interface IDocTree {
+
+}
