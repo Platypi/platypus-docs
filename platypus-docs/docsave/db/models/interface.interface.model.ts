a7e678163ad7b3c90c227e6c84805a4e48614c39
Donald Jones <donald@getplatypi.com>
2014-08-28 14:10:35 -0500

    PWS-103 #comment added interface-interface model.

---
+++ platypus-docs/docsave/db/models/interface.interface.model.ts
@@ -0,0 +1,18 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import errorModel = require('./error.model');
+import baseDoc = require('./api.model');
+
+export class InterfaceInterfaceModel extends baseDoc.ApiModel<IInterfaceInterface> {
+
+    /**
+     * Validates a Interface's Interface
+     */
+
+}
+
+export interface IInterfaceInterface extends baseDoc.IApiModel {
+    interfaceid?: number;
+    extendsinterfaceid?: number;
+}
+ 
