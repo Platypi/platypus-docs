ad8424a66f16f454b75d3d39fa14dbdfe55c234f
Donald Jones <donald@getplatypi.com>
2014-08-28 14:18:28 -0500

    PWS-103 #comment Added parameter model.

---
+++ platypus-docs/docsave/db/models/parameter.model.ts
@@ -0,0 +1,56 @@
+﻿/// <reference path="../../../typings/tsd.d.ts" />
+
+import errorModel = require('./error.model');
+import baseDoc = require('./api.model');
+
+export class ParameterModel extends baseDoc.ApiModel<IParameter> {
+
+    /**
+     * Validates a Parameter
+     */
+
+    validateProperties(parameter: IParameter): errorModel.IValidationErrors {
+        var validations = [
+            this.validateName(parameter.name),
+            this.validateDescription(parameter.description),
+            this.validateParentId(parameter)
+        ];
+        return super.filterValidations(validations);
+    }
+
+    // Individual Validations
+
+    validateName(name: string): errorModel.IValidationError {
+        return this.isString(name);
+    }
+
+    validateDescription(description: string): errorModel.IValidationError {
+        return this.isString(description);
+    }
+
+    validateParentId(p: IParameter): errorModel.IValidationError {
+        var n = this.isNumber,
+            t: number = 0;
+
+        if (!n(p.classtypeid, 'classtypeid')) {
+            t = p.classtypeid;
+        } else if (!n(p.methodtypeid, 'methodtypeid')) {
+            t = p.methodtypeid;
+        }
+        return n(t);
+    }
+}
+
+export interface IParameter extends baseDoc.IApiModel {
+    id?: number;
+    methodid?: number;
+    name?: string;
+    type?: string;
+    methodtypeid?: number;
+    classtypeid?: number;
+    interfacetypeid?: number;
+    description?: string;
+    defaultvalue?: string;
+    optional?: boolean;
+    porder?: string;
+}
