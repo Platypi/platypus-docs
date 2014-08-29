cff0a7f35d5dc6fd9bf5df9d878521edece96321
Donald Jones <donald@getplatypi.com>
2014-08-29 10:51:27 -0500

    Added connection to savetree method.

--- platypus-docs/storetree.ts
+++ platypus-docs/storetree.ts
@@ -1,16 +1,23 @@
-﻿/// <reference path="docnodes.ts" />
-/// <reference path="typings/tsd.d.ts" />
+﻿/// <reference path="typings/tsd.d.ts" />
+/// <reference path="docsave/db/db.d.ts" />
 
 import DocNodeTypes = require('./docnodes');
 import utils = require('./utils/utils');
+import connection = require('./docsave/db/connection');
 
 var saveDocTree = (tree: any) => {
     if (utils.isObject(tree) {
         for (var key in tree) {
             var currentNode: DocNodeTypes.INode = tree[key];
-
+            traverseNodeAndSave(currentNode);
         }
     } else {
         throw new Error('Invalid Doc Tree: ' + tree);
     }
-});
+};
+
+var traverseNodeAndSave = (node: DocNodeTypes.INode) => {
+
+};
+
+export = saveDocTree;
