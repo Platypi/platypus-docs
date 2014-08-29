f7847bde2e0eefb46c642e3951e6a68ca68b10b8
Donald Jones <donald@getplatypi.com>
2014-08-29 10:47:40 -0500

    PWS-103 #comment Initial Commit of Save Function

---
+++ platypus-docs/storetree.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="docnodes.ts" />
+/// <reference path="typings/tsd.d.ts" />
+
+import DocNodeTypes = require('./docnodes');
+import utils = require('./utils/utils');
+
+var saveDocTree = (tree: any) => {
+    if (utils.isObject(tree) {
+        for (var key in tree) {
+            var currentNode: DocNodeTypes.INode = tree[key];
+
+        }
+    } else {
+        throw new Error('Invalid Doc Tree: ' + tree);
+    }
+});
