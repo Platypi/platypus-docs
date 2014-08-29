072ede33994655a6642f83f0317ce90bf20b92e4
Donald Jones <donald@getplatypi.com>
2014-08-29 11:43:32 -0500

    Added procedures to storetree module.

--- platypus-docs/storetree.ts
+++ platypus-docs/storetree.ts
@@ -3,17 +3,35 @@
 
 import DocNodeTypes = require('./docnodes');
 import utils = require('./utils/utils');
-import connection = require('./docsave/db/connection');
+import PromiseStatic = require('es6-promise');
+
+// procedure types
+import apiprocedures = require('./docsave/db/procedures/api.procedures');
+import namespaceProcedure = require('./docsave/db/procedures/namespace.procedures');
+import interfaceProcedure = require('./docsave/db/procedures/interface.procedures');
+import classProcedures = require('./docsave/db/procedures/class.procedures');
+import methodProcedures = require('./docsave/db/procedures/method.procedures');
+import propertyProcedures = require('./docsave/db/procedures/property.procedures');
+import parameterProcedures = require('./docsave/db/procedures/parameter.procedures');
+import eventProcedures = require('./docsave/db/procedures/event.procedures');
+import classInterfaceProcedures = require('./docsave/db/procedures/class.interface.procedures');
+import interfaceInterfaceProcedures = require('./docsave/db/procedures/interface.interface.procedures');
+import typeParameterProcedures = require('./docsave/db/procedures/type.parameter.procedures');
+
 
 var saveDocTree = (tree: any) => {
     if (utils.isObject(tree) {
-        traverseNodeAndSave(tree);
+        saveAndTraverse(tree);
     } else {
         throw new Error('Invalid Doc Tree: ' + tree);
     }
 };
 
-var traverseNodeAndSave = (node: DocNodeTypes.INode) => {
+var saveAndTraverse = (node: DocNodeTypes.INode) => {
+    // save node
+    submitNode(node);
+
+    // process children
     if (node['namespaces']) {
     }
 
@@ -27,6 +45,37 @@ var traverseNodeAndSave = (node: DocNodeTypes.INode) => {
     }
 
     if (node['property']) {
+    }
+
+    if (node['event']) {
+    }
+};
+
+var submitNode = (node: DocNodeTypes.INode) => {
+    var procedure: any = null;
+    switch (node.kind) {
+        case 'namespace':
+            procedure = new namespaceProcedure();
+            procedure.create(node).then((id) => {
+                node.dbId = id;
+            });
+            break;
+        case 'interface':
+            
+            break;
+        case 'class':
+            break;
+        case 'method':
+            break;
+        case 'property':
+            break;
+        case 'event':
+            break;
+        case 'parameter':
+            break;
+        default:
+            throw new Error('unknown node type');
+            break;
     }
 };
 
