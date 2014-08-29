7509e2ee86f05a115cbfac37cf815ebae41c17aa
Donald Jones <donald@getplatypi.com>
2014-08-29 11:56:04 -0500

    PWS-103 #comment Added logic to traverse node types.

--- platypus-docs/storetree.ts
+++ platypus-docs/storetree.ts
@@ -33,45 +33,92 @@ var saveAndTraverse = (node: DocNodeTypes.INode) => {
 
     // process children
     if (node['namespaces']) {
+        var namespaces = node['namespaces'];
+        for (var n in namespaces) {
+            saveAndTraverse(namespaces[n]);
+        }
     }
 
     if (node['classes']) {
+        var classes = node['classes'];
+        for (var c in classes) {
+            saveAndTraverse(classes[c]);
+        }
     }
 
     if (node['methods']) {
+        var methods = node['methods'];
+        for (var m in methods) {
+            saveAndTraverse(methods[m]);
+        }
     }
 
     if (node['interfaces']) {
+        var interfaces = node['interfaces'];
+        for (var i in interfaces) {
+            saveAndTraverse(interfaces[i]);
+        }
     }
 
-    if (node['property']) {
+    if (node['properties']) {
+        var properties = node['properties'];
+        for (var p in properties) {
+            saveAndTraverse(properties[p]);
+        }
     }
 
-    if (node['event']) {
+    if (node['events']) {
+        var events = node['events'];
+        for (var e in events) {
+            saveAndTraverse(events[e]);
+        }
     }
 };
 
 var submitNode = (node: DocNodeTypes.INode) => {
-    var procedure: any = null;
+    var procedures: any = null;
     switch (node.kind) {
         case 'namespace':
-            procedure = new namespaceProcedure();
-            procedure.create(node).then((id) => {
+            procedures = new namespaceProcedure();
+            procedures.create(node).then((id) => {
                 node.dbId = id;
             });
             break;
         case 'interface':
-            
+            procedures = new interfaceProcedure();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         case 'class':
+            procedures = new classProcedures();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         case 'method':
+            procedures = new methodProcedures();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         case 'property':
+            procedures = new propertyProcedures();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         case 'event':
+            procedures = new eventProcedures();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         case 'parameter':
+            procedures = new parameterProcedures();
+            procedures.create(node).then((id) => {
+                node.dbId = id;
+            });
             break;
         default:
             throw new Error('unknown node type');
