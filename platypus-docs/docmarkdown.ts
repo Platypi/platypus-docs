729f2590d43256c1ce39589e4458547cf90e8376
Donald Jones <donald@getplatypi.com>
2014-09-11 11:28:19 -0500

    PWS-101 #comment Added basic logic for markdown handling of links.

--- platypus-docs/docmarkdown.ts
+++ platypus-docs/docmarkdown.ts
@@ -1,1 +1,9 @@
-﻿ 
+﻿var linkReg = /\{@link (.*?)[|](.*?)\}/g;
+
+var linkToMarkup = (content: string, baseURI: string, id: number): string => {
+    return content.replace(linkReg, (value, qualifiedPath, linkValue, index) => {
+        return '[' + qualifiedPath + '](' + linkValue + ')';
+    });
+};
+
+export = linkToMarkup;
