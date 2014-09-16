f7a957425627ce5ceee4aa86c2109e8a82049e3f
Donald Jones <donald@getplatypi.com>
2014-09-16 13:35:37 -0500

    PWS-101 #comment Fixed the link URL property.

--- platypus-docs/docmarkdown.ts
+++ platypus-docs/docmarkdown.ts
@@ -1,11 +1,33 @@
 ﻿import ds = require('./datastructures');
+import types = require('./docnodes');
 
 var linkReg = /\{@link (.*?)[|](.*?)\}/g;
 
 var linkToMarkup = (content: string, baseURI: string): string => {
-    // use namehash here
-    return content.replace(linkReg, (value, qualifiedPath, linkValue, index) => {
-        return '[' + qualifiedPath + '](' + linkValue + ')';
+    return content.replace(linkReg, (value: string, qualifiedPath: string, linkValue: string, index) => {
+        // build path using id/kind/name
+        var node: types.INode = ds.nameHashTable[qualifiedPath],
+            keyCount: number = Object.keys(ds.nameHashTable).length;
+
+        if (!node) {
+            node = ds.nameHashTable[qualifiedPath.toUpperCase()];
+        }
+
+        if (keyCount > 0) {
+            if (node && node.id) {
+                var path: string = baseURI + node.id + '/' + node.kind + '/' + node.name_;
+
+                return '[' + linkValue + '](' + path + ')';
+            } else {
+                console.log(qualifiedPath + ' not found');
+
+                return value;
+            }
+        } else {
+            console.log('namehash is empty');
+            return value;
+        }
+
     });
 };
 
