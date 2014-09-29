679538602b905eda09a3e39c83fe37dd9d59e49b
Donald Jones <donald@getplatypi.com>
2014-09-29 10:51:17 -0500

    PWS-172 #comment Added interface for new 'graphhandlers' abstraction.

---
+++ platypus-docs/generator/graphhandlers/graphhandler.interface.ts
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../_references.ts" />
+
+
+interface IGraphHandler {
+    handleGraphNodes: (flatObj: IFlatObject) => void;
+}
+
+interface IFlatObject {
+    [name: string]: INode;
+}
