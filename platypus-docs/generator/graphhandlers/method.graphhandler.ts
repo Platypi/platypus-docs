366883d7c7b47ff8da39d4907b9f19a02d65894d
Donald Jones <donald@getplatypi.com>
2014-09-29 12:00:11 -0500

    PWS-172 #comment Added Method Graph Handler

---
+++ platypus-docs/generator/graphhandlers/method.graphhandler.ts
@@ -0,0 +1,64 @@
+﻿/// <reference path="../../_references.ts" />
+
+import utils = require('../../utils/utils');
+import ds = require('../../variables/datastructures');
+
+class MethodGraphNodeHandler implements IGraphHandler {
+
+    constructor(private flatObj: { [name: string]: Array<IMethodNode> }) { }
+
+    handleGraphNodes(): void {
+        utils.forEach(this.flatObj, (value, key, obj) => {
+            var methodArray: Array<IMethodNode> = (<any>this.flatObj).methods;
+            utils.forEach(methodArray, (v, k, o) => {
+                var currentMethod = ds.flat.methods[key][k],
+                    returnTypeName = (typeof currentMethod.returntype === 'string' ? currentMethod.returntype : '');
+                
+                var returnTypeNode = ds.nameHashTable[returnTypeName];
+
+                if (returnTypeNode) {
+                    if (returnTypeNode === 'namespace') {
+                        currentMethod.returntypenamespace = returnTypeNode;
+                    } else if (returnTypeNode === 'class') {
+                        currentMethod.returntypeclass = returnTypeNode;
+                    } else if (returnTypeNode === 'interface') {
+                        currentMethod.returntypeinterface = returnTypeNode;
+                    }
+                }
+
+                ds.findNode(currentMethod, (node) => {
+                    var parentNode = node;
+                    currentMethod.parent = parentNode;
+
+                    utils.forEach(currentMethod.parameters, (v, k, o) => {
+                        var param: IParameterNode = currentMethod.parameters[k],
+                            resolvedType: INode = null;
+
+                        if (param.type) {
+                            resolvedType = ds.nameHashTable[param.type];
+                        }
+
+                        if (resolvedType) {
+                            var rKind = resolvedType.kind;
+
+                            if (rKind === 'method') {
+                                param.methodtype = resolvedType;
+                            } else if (rKind === 'class') {
+                                param.classtype = resolvedType;
+                            } else if (rKind === 'interface') {
+                                param.interfacetype = resolvedType;
+                            }
+
+                        }
+                        param.method = currentMethod;
+                        currentMethod.parameters[k] = param;
+                    });
+
+                    ds.appendChild(currentMethod, parentNode);
+                });
+            });
+        });
+    }
+}
+
+export = MethodGraphNodeHandler;
