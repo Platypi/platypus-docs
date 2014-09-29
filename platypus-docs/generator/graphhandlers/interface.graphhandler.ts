836cbfddb8572f78b539238b696427febde97021
Donald Jones <donald@getplatypi.com>
2014-09-29 11:13:27 -0500

    PWS-172 #comment Added Interface Graph Handler.

---
+++ platypus-docs/generator/graphhandlers/interface.graphhandler.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../../_references.ts" />
+
+import utils = require('../../utils/utils');
+import ds = require('../../variables/datastructures');
+
+class InterfaceGraphHandler implements IGraphHandler {
+    handleGraphNodes = (flatObj: IFlatObject): void => {
+        utils.forEach(flatObj, (value, key, obj) => {
+            var currentInterface: IInterfaceNode = flatObj[key];
+
+            if (currentInterface.memberof) {
+                ds.findNode(currentInterface, (node: INode) => {
+                    currentInterface.parent = node;
+
+                    if (!node) {
+                        return;
+                    }
+
+                    utils.forEach(currentInterface.interfaces, (v, k, o) => {
+                        var subinterface: IInterfaceNode = currentInterface.interfaces[k];
+                        currentInterface.interfaces[k] = ds.nameHashTable[subinterface.name_] || subinterface;
+                    });
+
+                    ds.appendChild(currentInterface, node);
+                });
+            } else {
+                ds.graph[currentInterface.name_] = currentInterface;
+            }
+        });
+    };
+}
+
+export = InterfaceGraphHandler;
