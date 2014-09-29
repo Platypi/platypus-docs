1567c7ce444bbc53f0c80c4d82e2f52eb281929b
Donald Jones <donald@getplatypi.com>
2014-09-29 10:51:41 -0500

    PWS-172 #comment Added base.graphhandler and namespace.graphhandler

---
+++ platypus-docs/generator/graphhandlers/base.graphhandler.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../../_references.ts" />
+
+import utils = require('../../utils/utils');
+import ds = require('../../variables/datastructures');
+
+class GraphNodeHandler implements IGraphHandler {
+    handleGraphNodes = (flatObj: IFlatObject): void => {
+        utils.forEach(flatObj, (value, key, obj) => {
+            var currentNode: INode = flatObj[key];
+
+            if (currentNode.memberof) {
+                ds.findNode(currentNode, (node: INode) => {
+                    currentNode.parent = node;
+
+                    if (!node) {
+                        return;
+                    }
+
+                    ds.appendChild(currentNode, node);
+                });
+            } else {
+                ds.graph[currentNode.name_] = currentNode;
+            }
+        });
+    };
+}
+
+export = GraphNodeHandler;
---
+++ platypus-docs/generator/graphhandlers/namespace.graphhandler.ts
@@ -0,0 +1,7 @@
+﻿import BaseGraphNodeHandler = require('./base.graphhandler');
+
+class NamespaceGraphNodeHandler extends BaseGraphNodeHandler {
+
+}
+
+export = NamespaceGraphNodeHandler;
