96e129728e21946a09e19d5c4d2ed5d3f0abda53
Donald Jones <donald@getplatypi.com>
2014-09-30 15:12:23 -0500

    PWS-173 #comment Added pub/sub variables.

--- platypus-docs/variables/globals.ts
+++ platypus-docs/variables/globals.ts
@@ -3,8 +3,14 @@
  * Set by command line arguments.
  */
 
+import EventEmitter = require('events');
+
 export var linkBase = '/#!/docs/api/';
 
 export var versionNumber = '0.0.1';
 
 export var debug = false;
+
+export var pubsub = new EventEmitter.EventEmitter();
+
+export var nodeCount = 0;
