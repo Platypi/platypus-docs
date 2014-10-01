f33bec14a2584f9451253b1c9c46e42c9999f0cb
Donald Jones <donald@getplatypi.com>
2014-10-01 12:13:58 -0500

    PWS-173 #comment Added configuration prompt to grunt for dbconnection.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,42 @@ module.exports = function (grunt) {
     ];
 
     grunt.initConfig({
+        prompt: {
+            database: {
+                options: {
+                    questions: [
+                        {
+                            config: 'server.selection',
+                            type: 'list',
+                            message: 'Generate docs on which server: ',
+                            choices: [
+                                {
+                                    value: {
+                                        host: 'us-cdbr-azure-east-a.cloudapp.net',
+                                        dbName: 'platypiwebdev',
+                                        user: 'b091fc43c1a652'
+                                    },
+                                    name: 'DEV (platypiwebdev)'
+                                },
+                                {
+                                    value: {
+                                        host: 'us-cdbr-azure-east-a.cloudapp.net',
+                                        dbName: 'platypiweb',
+                                        user: 'b41c2115ab2d1a'
+                                    },
+                                    name: 'PRODUCTION (platypiweb)'
+                                }
+                            ]
+                        },
+                        {
+                            config: 'server.password',
+                            type: 'input',
+                            message: 'Password:'
+                        }
+                    ]
+                }
+            }
+        },
         typescript: {
             options: {
                 module: 'commonjs',
@@ -23,8 +59,34 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-prompt');
+
+    grunt.registerTask('saveDbConfig', 'Output a db json file.', function () {
+        var selection = grunt.config('server.selection'),
+            password = grunt.config('server.password'),
+            db = {
+                database: {
+                    host: selection.host,
+                    user: selection.user,
+                    dbName: selection.dbName,
+                    password: password
+                }
+            };
+
+        grunt.file.write('dbconnection.json', JSON.stringify(db));
+
+        grunt.log.writeln('Saved dbconnection.json');
+    });
+
+    grunt.registerTask('configure', ['prompt:database', 'saveDbConfig']);
+
+    grunt.registerTask('checkForDbConfig', "Check for dbconnection.json", function () {
+        if (!grunt.file.isFile('./dbconnection.json')) {
+            grunt.task.run(['configure']);
+        }
+    });
 
     grunt.registerTask('build', ['shell', 'typescript'])
 
-    grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build', 'checkForDbConfig']);
 };
