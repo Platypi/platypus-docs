aa88741d6ee0779cc8fe6698bce7865d261a69e5
Donald Jones <donald@getplatypi.com>
2014-10-01 17:04:16 -0500

    PWS-175 #comment Added 'generate' grunt task that will generate db scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,6 +39,41 @@ module.exports = function (grunt) {
                         }
                     ]
                 }
+            },
+            scriptGen: {
+                options: {
+                    questions: [
+                        {
+                            config: 'server.scripts',
+                            type: 'confirm',
+                            message: 'Do you want to generate MySql Schema Scripts?'
+                        },
+                        {
+                            config: 'server.host',
+                            type: 'input',
+                            message: 'Server hostname:',
+                            when: function (answers) {
+                                return answers['server.scripts'];
+                            }
+                        },
+                        {
+                            config: 'server.dbName',
+                            type: 'input',
+                            message: 'Database name:',
+                            when: function (answers) {
+                                return answers['server.scripts'];
+                            }
+                        },
+                        {
+                            config: 'server.userName',
+                            type: 'input',
+                            message: 'Username:',
+                            when: function (answers) {
+                                return answers['server.scripts'];
+                            }
+                        }
+                    ]
+                }
             }
         },
         typescript: {
@@ -76,6 +111,37 @@ module.exports = function (grunt) {
         grunt.file.write('dbconnection.json', JSON.stringify(db));
 
         grunt.log.writeln('Saved dbconnection.json');
+
+    });
+
+    grunt.registerTask('generateSchemaFiles', 'Generates DB Schema files.', function () {
+        var scripts = grunt.config('server.scripts'),
+            selection = {
+                host: grunt.config('server.host'),
+                user: grunt.config('server.userName'),
+                dbName: grunt.config('server.dbName')
+            };
+
+        if (scripts) {
+            var tables = grunt.file.read('./platypus-docs/docsave/db/schema/tables/tables.txt'),
+                procedures = grunt.file.read('./platypus-docs/docsave/db/schema/storedprocedures/storedprocedures.txt');
+
+            tables = tables.replace(/%dbName%/g, selection.dbName)
+                .replace(/%hostName%/g, selection.host)
+                .replace(/%userName%/g, selection.user);
+
+            procedures = procedures.replace(/%dbName%/g, selection.dbName)
+                .replace(/%hostName%/g, selection.host)
+                .replace(/%userName%/g, selection.user);
+
+            grunt.file.mkdir('schema');
+
+            grunt.file.write('./schema/tables.sql', tables);
+            grunt.log.writeln('Created tables.sql');
+
+            grunt.file.write('./schema/storedprocedures.sql', procedures);
+            grunt.log.writeln('Created storedprocedures.sql');
+        }
     });
 
     grunt.registerTask('configure', ['prompt:database', 'saveDbConfig']);
@@ -85,6 +151,8 @@ module.exports = function (grunt) {
             grunt.task.run(['configure']);
         }
     });
+
+    grunt.registerTask('generate', ['prompt:scriptGen', 'generateSchemaFiles']);
 
     grunt.registerTask('build', ['shell', 'typescript']);
 
