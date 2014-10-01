722403fc1a6247df28f50bf36ae0fd61ddb19721
Donald Jones <donald@getplatypi.com>
2014-10-01 09:34:33 -0500

    PWS-173 #comment Added event listener for errors.

---
+++ platypus-docs/events/error.event.ts
@@ -0,0 +1,10 @@
+﻿import globals = require('../variables/globals');
+
+var setupEvent = () => {
+    globals.pubsub.on('error', (err) => {
+        globals.runTimeErrors.push(err);
+    });
+};
+
+export = setupEvent;
+  
